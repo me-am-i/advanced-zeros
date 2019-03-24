8fe16580225a3278d4274e48d1d342f19e6f8e0d
dgm <vsl_sv@bk.ru>
2019-03-24 23:32:50 +0300

    feat: add part one

--- src/index.js
+++ src/index.js
@@ -1,3 +1,96 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+
+//разложение числа на простые множители
+
+
+var array = [];
+var osnovanie = base;
+
+if (base % 2 == 0) {
+  console.log (base + " чётное");
+  
+  osnovanie = base / 2;
+
+  array[array.length] = 2; 
+ 
+  return 2;
+
+}
+
+if (osnovanie % 3 == 0)  {
+  console.log (base + " нечётное");
+
+  osnovanie = base / 3;
+
+  array[array.length] = 3; 
+
+} 
+
+if (osnovanie % 5 == 0) {
+  console.log (base + " чётное");
+  
+  osnovanie = base / 5;
+
+  array[array.length] = 5; 
+ 
+
+}
+
+if (osnovanie % 7 == 0)  {
+  console.log (base + " нечётное");
+
+  osnovanie = base / 7;
+
+  array[array.length] = 7; 
+
+} 
+
+
+
+
+console.log(array);
+
+
+
+
+
+
+
+
+if (false) {
+
+
+console.log (number, base);
+
+
+nulls (number, base);
+
+
+
+
+function nulls () { //считаем нули в факториале 
+
+ let nulls = 0;
+ let count = 1;
+ 
+ while (Math.pow(base, count) < number) {
+ nulls = nulls +  Math.pow(base, count);
+ count++;
+ } 
+
+ console.log ("нулей - " + nulls + " факториал числа " + number);
+
+
+
+
+
+ // [n/p] + [n/p^2] + [n/p^3] + [n/p^4]
+
+
+}
+
+}
+
+
 }
